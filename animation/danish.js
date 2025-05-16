037669224a61e551991c04151c302d969487b61d
danishansaris04 <danishansariserver@gmail.com>
2025-05-16 19:55:24 +0530

    Create danish.js

---
+++ animation/danish.js
@@ -0,0 +1,9 @@
+const movies = [
+
+  {
+    "name": "3 Idiots (2009)",
+    "link": "https://v2.sdsp.xyz/embed/movie/20453",
+    "poster": "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/66A9MqXOyVFCssoloscw79z8Tew.jpg"
+  },
+
+  ]
