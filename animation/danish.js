13f218a58a83f2fad23d99fa8e8309e5a441d799
danishansaris04 <danishansariserver@gmail.com>
2025-05-16 20:00:45 +0530

    Update danish.js

--- animation/danish.js
+++ animation/danish.js
@@ -1,9 +1,49 @@
 const movies = [
-
   {
-    "name": "3 Idiots (2009)",
-    "link": "https://v2.sdsp.xyz/embed/movie/20453",
-    "poster": "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/66A9MqXOyVFCssoloscw79z8Tew.jpg"
+    "name": "How to Train Your Dragon (2010)",
+    "link": "https://v2.sdsp.xyz/embed/movie/12345",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/dragon1.jpg"
   },
+  {
+    "name": "How to Train Your Dragon 2 (2014)",
+    "link": "https://v2.sdsp.xyz/embed/movie/12346",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/dragon2.jpg"
+  },
+  {
+    "name": "How to Train Your Dragon: The Hidden World (2019)",
+    "link": "https://v2.sdsp.xyz/embed/movie/12347",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/dragon3.jpg"
+  },
+  {
+    "name": "Up (2009)",
+    "link": "https://v2.sdsp.xyz/embed/movie/8505",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/up2009.jpg"
+  },
+  {
+    "name": "Migration",
+    "link": "https://v2.sdsp.xyz/embed/movie/23456",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/migration.jpg"
+  },
+  {
+    "name": "Bee Movie (2007)",
+    "link": "https://v2.sdsp.xyz/embed/movie/12348",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/beemovie.jpg"
+  },
+  {
+    "name": "Angry Birds (2016)",
+    "link": "https://v2.sdsp.xyz/embed/movie/12349",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/angrybirds1.jpg"
+  },
+  {
+    "name": "Angry Birds 2 (2019)",
+    "link": "https://v2.sdsp.xyz/embed/movie/12350",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/angrybirds2.jpg"
+  },
+  {
+    "name": "The Boss Baby (2017)",
+    "link": "https://v2.sdsp.xyz/embed/movie/12351",
+    "poster": "https://image.tmdb.org/t/p/w300_and_h450_bestv2/bossbaby.jpg"
+  }
+
 
   ]
