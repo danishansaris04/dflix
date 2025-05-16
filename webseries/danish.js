f1ebfb41219ef990834e288cfbeeb732b0d06986
danishansaris04 <danishansariserver@gmail.com>
2025-05-16 15:00:06 +0530

    Create danish.js

---
+++ webseries/danish.js
@@ -0,0 +1,25 @@
+ const seriesData = {
+      "Farzi": {
+        imdb_id: "tt15477488",
+        seasons: [
+          {
+            number: 1,
+            title: "Season 1",
+            episodes: [
+              { name: "Episode 1", ep: 1 },
+              { name: "Episode 2", ep: 2 },
+              { name: "Episode 3", ep: 3 },
+              { name: "Episode 4", ep: 4 }
+            ]
+          },
+          {
+            number: 2,
+            title: "Season 2",
+            episodes: [
+              { name: "Episode 1", ep: 1 },
+              { name: "Episode 2", ep: 2 }
+            ]
+          }
+        ]
+      }
+    };
