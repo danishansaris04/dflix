b1ceb9c8b50eb560dd9271ad1036a4caf1ce6438
danishansaris04 <danishansariserver@gmail.com>
2025-05-16 17:34:37 +0530

    Update danish.js

--- webseries/danish.js
+++ webseries/danish.js
@@ -23,28 +23,25 @@ const movies = [
 
 const seriesData = {
   "Farzi": {
-    imdb_id: "tt15477488",
-    seasons: [
-      {
-        number: 1,
-        title: "Season 1",
-        episodes: [
-          { name: "Episode 1", ep: 1 },
-          { name: "Episode 2", ep: 2 },
-          { name: "Episode 3", ep: 3 },
-          { name: "Episode 4", ep: 4 }
-        ]
-      },
-      {
-        number: 2,
-        title: "Season 2",
-        episodes: [
-          { name: "Episode 1", ep: 1 },
-          { name: "Episode 2", ep: 2 }
-        ]
-      }
-    ]
-  },
+"Farzi": {
+  imdb_id: "tt15477488",
+  seasons: [
+    {
+      number: 1,
+      title: "Season 1",
+      episodes: [
+        { name: "Episode 1", ep: 1 },
+        { name: "Episode 2", ep: 2 },
+        { name: "Episode 3", ep: 3 },
+        { name: "Episode 4", ep: 4 },
+        { name: "Episode 5", ep: 5 },
+        { name: "Episode 6", ep: 6 },
+        { name: "Episode 7", ep: 7 },
+        { name: "Episode 8", ep: 8 }
+      ]
+    }
+  ]
+},
 
   "Mirzapur": {
     imdb_id: "tt6473300",
